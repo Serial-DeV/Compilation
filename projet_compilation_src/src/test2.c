1350c147e2d9dd18034b9c29197bc06a33a012a1
3677248 <rene-mervis.mudry@etu.upmc.fr>
2020-05-21 16:56:57 +0200

    Mini modif

--- projet_compilation_src/src/test2.c
+++ projet_compilation_src/src/test2.c
@@ -4,5 +4,7 @@ void main()
 	int c;
 	//c = 5 ?
 	// c = 3 + 2;
-	int a, d, f, d, h;
+	int a, d, f, d, h = 6; //Corriger le problème
 }
+
+//Verifier pour les noeuds qui manquent, notamment le print
