0cf6dee083760f919ef7b7f8f761e76c28eca1ed
3677248 <rene-mervis.mudry@etu.upmc.fr>
2020-05-22 11:10:38 +0200

    Correction commentaire dans lex

--- projet_compilation_src/src/test2.c
+++ projet_compilation_src/src/test2.c
@@ -1,10 +1,6 @@
 void main()
 {
 	int f = 5;
-	int c;
-	//c = 5 ?
-	// c = 3 + 2;
-	int a, d, f, d, h = 6; //Corriger le problème
+	//Verifier pour les noeuds qui manquent, notamment le print
+	// int a, d, f, d, h = 6 Corriger le problème
 }
-
-//Verifier pour les noeuds qui manquent, notamment le print
